b4d5d8849c90c3729829fc216e95bb24c5ab10b9
ejinaaa <eunjink0423@gmail.com>
2020-11-12 12:41:55 +0900

    bug: fix some errors shown on the pages

--- js/brand-value.js
+++ js/brand-value.js
@@ -2,12 +2,12 @@ const item1 = document.getElementsByClassName("brand-value__lab-item").item(0);
 const item2 = document.getElementsByClassName("brand-value__lab-item").item(1);
 
 item1.addEventListener("mouseenter", function(){
-    item1.classList.remove('down');
-    item1.classList.add('up');
-    item2.classList.remove('up');
+    item1.classList.remove('back');
+    item1.classList.add('front');
+    item2.classList.remove('front');
 })
 item2.addEventListener("mouseenter", function(){
-    item2.classList.remove('down');
-    item2.classList.add('up');
-    item1.classList.remove('up');
+    item2.classList.remove('back');
+    item2.classList.add('front');
+    item1.classList.remove('front');
 })
